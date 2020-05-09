2ea3c1941f4caa5e8d69940b06e40a7d127fab54
Jasery <jaserydev@163.com>
2020-05-09 23:08:54 +0800

    feat: 添加应用头部

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import {renderRoutes} from "react-router-config";
-
+import {Tab, TabItem, Top} from "./style";
+import {NavLink} from "react-router-dom";
 
 function Home(props) {
   const { route } = props;
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">Cloud Music</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected">
+          <TabItem><span>推举</span></TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeClassName="selected">
+          <TabItem><span>歌手</span></TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeClassName="selected">
+          <TabItem><span>排行榜</span></TabItem>
+        </NavLink>
+      </Tab>
       { renderRoutes(route.routes) }
     </div>
   )
